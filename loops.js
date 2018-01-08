39b04a7c6ddec5f15f28e2244f9d5add68cf92bb
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-08 16:27:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,9 +19,9 @@ function whileLoop(number) {
 function doWhileLoop(array) {
   function maybeTrue() {
   return Math.random() >= 0.5
-}
+  }
  
-do {
-  array.pop()
-} while (array.length > 0 && maybeTrue());
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue());
 }
