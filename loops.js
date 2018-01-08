8b47f471638859ce6ec7c6117015026bde31d988
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-08 16:18:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,8 +10,9 @@ function forLoop(array) {
 }
 
 function whileLoop(number) {
-  while(number >= 0) {
-    console.log(number -= 1)
+  while(number > 0) {
+    console.log(number);
+    number -=1
   }
   return ("done")
 }
